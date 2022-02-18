c1e653dbe4c31b8ba2578875e06e9383950bdfb1
Hima Dionysus Balde <ibalde@isitymarket.onmicrosoft.com>
2022-02-18 14:28:47 -0500

    init dev branch; rmdir of main

--- config/index.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv'
-
-dotenv.config();
-export default {
-  db: process.env.DB,
-  jwtSecret: process.env.JWT_SECRET,
-  port: process.env.PORT,
-  allowedOrigins: ['http://localhost:3000','http://youapp.com', 'http://localhost:4020']
-};
